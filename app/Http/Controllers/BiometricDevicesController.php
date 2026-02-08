0e91a672d1a3ea3300055b372df1f96558f6b3f2
Nicers <ata10plus10@gmail.com>
2026-02-08 20:46:31 +0500

    1. Biometric Device model created.

---
+++ app/Http/Controllers/BiometricDevicesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BiometricDevices;
+use Illuminate\Http\Request;
+
+class BiometricDevicesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BiometricDevices $biometricDevices)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BiometricDevices $biometricDevices)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BiometricDevices $biometricDevices)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BiometricDevices $biometricDevices)
+    {
+        //
+    }
+}
